912f57adffa94b20cb81816bb6e016bdc735a400
Alfredo Bejarano <acorona@Alfredo-Corona.local>
2015-09-23 14:16:09 -0500

    Añadida la posibilidad de buscar dispositivos, no más HC-06 hardcodeado :bowtie:

---
+++ app/src/main/java/me/alfredobejarano/carduinorc/MainActivity.java
@@ -0,0 +1,165 @@
+package me.alfredobejarano.carduinorc;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView lista;
+    private Button botonBusqueda;
+    private int backButtonCount = 0;
+    private ArrayAdapter adaptadorLista;
+    private Set<BluetoothDevice> emparejados;
+    private ArrayList<BluetoothDevice> encontrados;
+    private ArrayList<String> nombresEmparejados = new ArrayList<>();
+    private ArrayList<String> nombresEncontrados = new ArrayList<>();
+    private ArrayList<String> nombres = new ArrayList<>();
+    private final BluetoothAdapter adaptadorBt = BluetoothAdapter.getDefaultAdapter();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lista = (ListView) findViewById(R.id.lista);
+        botonBusqueda = (Button) findViewById(R.id.buttonSearch);
+        Intent i;
+
+        Intent gi =  getIntent();
+        encontrados = obtenerEncontrados(gi);
+
+
+        if(!adaptadorBt.isEnabled()) {
+            adaptadorBt.enable();
+            i = new Intent(MainActivity.this, MainActivity.class);
+            startActivity(i);
+        } else {
+            emparejados = obtenerEmparejados(adaptadorBt);
+
+            if(emparejados.size() > 0) {
+                nombresEmparejados = llenarNombres(emparejados);
+                nombres.addAll(nombresEmparejados);
+            }
+
+            if (encontrados != null) {
+                if(encontrados.size() > 0) {
+                    nombresEncontrados = llenarNombres(encontrados);
+                    nombres.addAll(nombresEncontrados);
+                }
+            }
+
+            if (nombres.size() <= 0) {
+                nombres.add("No se encontrarón dispositivos :(");
+            }
+
+
+            adaptadorLista = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, nombres);
+            lista.setAdapter(adaptadorLista);
+
+            botonBusqueda.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(MainActivity.this, SearchDevices.class);
+                    if(!adaptadorBt.isEnabled()) {
+                        adaptadorBt.enable();
+                    }
+                    startActivity(i);
+                }
+            });
+        }
+
+    }
+
+    private Set<BluetoothDevice> obtenerEmparejados(BluetoothAdapter adaptadorBt) {
+        Set<BluetoothDevice> emparejados = adaptadorBt.getBondedDevices();
+        return emparejados;
+    }
+
+    private ArrayList<String> llenarNombres(Set<BluetoothDevice> dispositivos) {
+        ArrayList<String> nombres = new ArrayList<>();
+        String nombre;
+
+        for(BluetoothDevice dispositivo : dispositivos) {
+            nombre = dispositivo.getName();
+
+            if((nombre == null) || (nombre.isEmpty())) {
+                nombre = "Dispositivo sin nombre";
+            }
+
+            nombres.add(nombre);
+        }
+
+        return nombres;
+    }
+
+    private ArrayList<String> llenarNombres(ArrayList<BluetoothDevice> dispositivos) {
+        ArrayList<String> nombres = new ArrayList<>();
+        String nombre;
+
+        for(BluetoothDevice dispositivo : dispositivos) {
+            nombre = dispositivo.getName();
+
+            if((nombre == null) || (nombre.isEmpty())) {
+                nombre = "Dispositivo sin nombre";
+            }
+
+            nombres.add(nombre);
+        }
+
+        return nombres;
+    }
+
+    private ArrayList<BluetoothDevice> obtenerEncontrados(Intent i) {
+        ArrayList<BluetoothDevice> dispositivosEncontrados = i.getParcelableArrayListExtra("dispositivosEncontrados");
+        return dispositivosEncontrados;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(backButtonCount >= 1)
+        {
+            android.os.Process.killProcess(android.os.Process.myPid());
+        }
+        else
+        {
+            Toast.makeText(this, "Presiona de nuevo para salir", Toast.LENGTH_SHORT).show();
+            backButtonCount++;
+        }
+    }
+}
---
+++ app/src/main/java/me/alfredobejarano/carduinorc/SearchDevices.java
@@ -0,0 +1,68 @@
+package me.alfredobejarano.carduinorc;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.camera2.params.BlackLevelPattern;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class SearchDevices extends AppCompatActivity {
+
+    private TextView titulo;
+    private IntentFilter filtro = new IntentFilter();
+    private final BluetoothAdapter adaptadorBt = BluetoothAdapter.getDefaultAdapter();
+    private ArrayList<BluetoothDevice> dispositivosEncontrados = new ArrayList<>();
+    private final BroadcastReceiver receptorBt = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            titulo = (TextView) findViewById(R.id.bluetoothdevices);
+            String estado = intent.getAction();
+            if (BluetoothDevice.ACTION_FOUND.equals(estado)) {
+                BluetoothDevice dispositivoEncontrado = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                titulo.setText(titulo.getText()+".");
+                dispositivosEncontrados.add(dispositivoEncontrado);
+            }
+
+            if(BluetoothAdapter.ACTION_DISCOVERY_STARTED.equals(estado)) {
+                Toast.makeText(context, "Busqueda iniciada", Toast.LENGTH_SHORT);
+            }
+
+            if(BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(estado)) {
+                Toast.makeText(context,"Busqueda Finalizada", Toast.LENGTH_SHORT);
+                Intent i = new Intent(SearchDevices.this, MainActivity.class);
+                Log.d("","\n"+dispositivosEncontrados.size());
+                i.putParcelableArrayListExtra("dispositivosEncontrados", dispositivosEncontrados);
+                startActivity(i);
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_devices);
+
+        filtro.addAction(BluetoothDevice.ACTION_FOUND);
+        filtro.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filtro.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        this.registerReceiver(receptorBt, filtro);
+        adaptadorBt.startDiscovery();
+    }
+
+    @Override
+    public void onBackPressed() {
+        Toast.makeText(this, "No puedes regresar mientras una búsqueda esta en curso", Toast.LENGTH_SHORT).show();
+    }
+}
